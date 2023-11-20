fe64ba23c55e893fa05233eab85320e9687ec784
¨JaviZaldu¨ <javizaldu9@gmail.com>
2023-11-20 16:50:58 -0300

    Pagina completa

--- Client/src/index.js
+++ Client/src/index.js
@@ -7,6 +7,12 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./Redux/Store";
 
+// import axios from "axios";
+
+// axios.defaults.baseURL = "http://localhost:3001";
+
+// axios.defaults.baseURL = "portfoliojz-production.up.railway.app";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
