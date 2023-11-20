20d75116cef46997ef755abbfa20608b61a01dbb
¨JaviZaldu¨ <javizaldu9@gmail.com>
2023-11-20 17:55:13 -0300

    conectado

--- Client/src/Redux/Actions.js
+++ Client/src/Redux/Actions.js
@@ -7,7 +7,7 @@ export function sendEmail(subject, name, lastName, email, message) {
   return async function (dispatch) {
     try {
       const response = await axios.post(
-        "http://localhost:3001/portaldistribuidora/email",
+        "/portaldistribuidora/email",
         {
           subject,
           name,
